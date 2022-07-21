f28e318a60827b0708d884327ae5204cb612c8d2
Q040528 <109710844+Q040528@users.noreply.github.com>
2022-07-21 16:06:24 +0800

    加阶版，10！和的计算

--- 未命名.c
+++ 未命名.c
@@ -1,12 +1,17 @@
 # include <stdio.h>
 int main()
 {
-	int n,m=1,i=1;
-	scanf("%d",&n);
-	while(i<=n)
+	int n=1,m=1,i=1,sum=0;
+	while(n<11)
 	{
-		m=m*i;
-		i++;
-	}
-	printf("n的阶乘为：%d\n",m);
+		 while(i<=n)
+		{
+			m=m*i;
+			i++;
+		}
+		printf("n的阶乘为：%d\n",m);
+		sum+=m;
+		n++;	 
+	} 
+        printf("10的阶乘和为：%d\n",sum);   
 }
